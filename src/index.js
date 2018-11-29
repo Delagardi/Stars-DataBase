cbe35d55624e5c5a8a4eb63b59e8fc59b0a60f52
Delagardi <delagardi@ymail.com>
2018-11-30 00:20:45 +0200

    Added prototype of client API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class SwapiServices {
+  _apiBase = 'https://swapi.co/api';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  async getResourse(url) {
+    
+    const response = await fetch(`${this._apiBase}${url}`);
+
+    if (!response.ok) {
+      throw new Error(`We couldn't FETCH url ${url}. We get error: ${response.status}`);
+    }
+
+    const body = await response.json();
+
+    return body;
+  }
+
+  async getPeopleAll() {
+    const response = await this.getResourse(`/people/`);
+    return response.results;
+  }
+
+  async getPeopleById(id) {
+    const response = await this.getResourse(`/people/${id}`);
+    return response;
+  }
+
+  async getPlanetsAll() {
+    const response = await this.getResourse(`/planets/`);
+    return response.results;
+  }
+
+  async getPlanetById(id) {
+    const response = await this.getResourse(`/planets/${id}`);
+    return response;
+  }
+
+  async getStarshipsAll() {
+    const response = await this.getResourse(`/starships/`);
+    return response.results;
+  }
+
+  async getStarshipsById(id) {
+    const response = await this.getResourse(`/starships/${id}`);
+    return response;
+  }
+}
+
+const swapi = new SwapiServices();
+
+swapi.getPeopleAll()
+  .then( (data) => {
+    data.forEach( ( hero ) => {
+      console.log( hero.name );
+    });
+  });
+
+const needleId = 3;
+
+swapi.getPeopleById(needleId)
+  .then( (data) => {
+    console.log(`Hero with id: ${needleId}, is ${data.name}`);
+  });
